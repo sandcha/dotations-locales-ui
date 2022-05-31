c62870453943b73c3b6ac10b7db279dafe572b32
sandcha <sandcha@users.noreply.github.com>
2022-05-31 18:09:33 +0200

    Ajoute un Footer au format Figma

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+</script>
+
+<div>
+  <img src="logo-republique-fr.svg" alt="Logo de la République Française" />
+  <img
+    src="logo-incubateur-territoires.svg"
+    alt="Logo de l'Incubateur des Territoires"
+  />
+</div>
+
+<style>
+  /* Pied de page - Complet */
+  div {
+    /* Auto layout */
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    padding: 0px;
+
+    width: 1440px;
+    height: 287px;
+
+    /* Inside auto layout */
+    flex: none;
+    order: 6;
+    flex-grow: 0;
+  }
+</style>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
     import "../app.css"
     import Navbar from "$lib/components/Navbar.svelte";
+    import Footer from "$lib/components/Footer.svelte";
 </script>
 
 <Navbar/>
 
 <slot></slot>
+
+<Footer/>
