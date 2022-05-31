cefe3a5758d4ed697e62ac5ec09a352f3ca4b9d2
sandcha <sandcha@users.noreply.github.com>
2022-05-31 11:47:35 +0200

    Ajoute les dépendances minimales Svelte

---
+++ svelte.config.js
@@ -0,0 +1,37 @@
+import node from "@sveltejs/adapter-node"
+import preprocess from "svelte-preprocess"
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  kit: {
+    // By default, `npm run build` will create a standard Node app.
+    // You can create optimized builds for different platforms by
+    // specifying a different adapter
+    adapter: node({
+      precompress: true,
+    }),
+
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: "#svelte",
+
+    vite: {
+      build: {
+        // Size of chunks increased to 3 MB, to be able to import JSON files (reduce ?).
+        chunkSizeWarningLimit: 3072,
+      },
+      ssr: {
+        noExternal: ["d3-scale"],
+      },
+    },
+  },
+
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: [
+    preprocess({
+      postcss: true,
+    }),
+  ],
+}
+
+export default config
