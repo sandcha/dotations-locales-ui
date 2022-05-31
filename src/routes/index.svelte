a12faaa7d023af9de69ea89808c7366ad5bdb080
sandcha <sandcha@users.noreply.github.com>
2022-05-31 18:09:33 +0200

    Ajoute un mock de recherche

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,9 +6,30 @@
 </svelte:head>
 
 <main>
-    <section>toto</section>
+  <section>
+    Rechercher votre collectivité
+    <textarea value="Chauffailles" />
+  </section>
 </main>
 
 <style>
+  section {
+    box-sizing: border-box;
 
+    /* Auto layout */
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 80px 0px;
+    gap: 24px;
+
+    width: 1440px;
+    height: 336px;
+
+    /* Inside auto layout */
+    flex: none;
+    order: 1;
+    align-self: stretch;
+    flex-grow: 0;
+  }
 </style>
