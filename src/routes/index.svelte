6e1c8cc67be2d8e00126dc2bb4a81160333315f4
sandcha <sandcha@users.noreply.github.com>
2022-05-31 15:30:26 +0200

    Ajoute une barre de navigation suivant Figma

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,9 +2,42 @@
 </script>
 
 <svelte:head>
-  <title>bonjour :)</title>
+  <title>dotations locales</title>
 </svelte:head>
 
-<main>coucou</main>
+<main>
+  <nav>
+    <img src="logo-republique-fr.svg" alt="Logo de la République Française"/>
+    <ul>
+      <li>Notre mission</li>
+      <li>Contact</li>
+    </ul>
+  </nav>
+</main>
 
-<style></style>
+<style>
+  /* 📁 Contenant technique - En-tête */
+  nav {
+    /* Auto layout */
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    padding: 12px 120px 12px 108px;
+    gap: 40px;
+
+    width: 1440px;
+    height: 102px;
+
+    /* light/background/elevated-grey */
+    background: #ffffff;
+
+    /* Inside auto layout */
+    flex: none;
+    order: 0;
+    flex-grow: 1;
+  }
+
+  ul {
+    list-style-image: url("icone-fleche-droite.svg");
+  }
+</style>
