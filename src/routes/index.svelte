cd8e23c6b5023f9e08c5af973c2013d4c238ec41
sandcha <sandcha@users.noreply.github.com>
2022-05-31 18:59:42 +0200

    Ajoute le css Figma à la section de recherche

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,13 +7,25 @@
 
 <main>
   <section>
-    Rechercher votre collectivité
-    <textarea value="Chauffailles" />
+    <p>Rechercher votre collectivité</p>
+    <div id="recherche">
+      <div id="quoi">
+        <textarea value="Chauffailles" />
+      </div>
+      <div id="button">
+        <button>
+          <p id="button">Rechercher</p>
+        </button>
+      </div>
+      <hr />
+    </div>
+    <p id="detail-recherche">détail recherche</p>
   </section>
 </main>
 
 <style>
   section {
+    /* SECTION */
     box-sizing: border-box;
 
     /* Auto layout */
@@ -30,6 +42,210 @@
     flex: none;
     order: 1;
     align-self: stretch;
+    flex-grow: 0;
+  }
+
+  p {
+    /* Rechercher votre collectivité */
+
+    width: 560px;
+    height: 48px;
+
+    /* 1.Titres/H1 - XL/Desktop */
+    font-family: "Marianne-Bold";
+    font-style: normal;
+    font-weight: 700;
+    font-size: 40px;
+    line-height: 48px;
+    /* identical to box height, or 120% */
+    text-align: center;
+
+    /* light/text/title-grey */
+    color: #161616;
+
+    /* Inside auto layout */
+    flex: none;
+    order: 0;
+    flex-grow: 0;
+  }
+
+  div#recherche {
+    /* Barre de recherche
+
+    Barre permettant d'accéder rapidement à du contenu.
+    */
+
+    /* Auto layout */
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+    padding: 0px;
+
+    width: 792px;
+    height: 56px;
+
+    /* Inside auto layout */
+    flex: none;
+    order: 1;
+    flex-grow: 0;
+  }
+
+  div#quoi {
+    /* Champ */
+
+    /* Auto layout */
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    padding: 0px;
+
+    width: 585px;
+    height: 56px;
+
+    /* light/background/contrast-grey */
+    background: #eeeeee;
+    border-radius: 4px 0px 0px 0px;
+
+    /* Inside auto layout */
+    flex: none;
+    order: 0;
+    flex-grow: 1;
+  }
+
+  textarea {
+    /* Saisie */
+
+    /* Auto layout */
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+    padding: 16px;
+    gap: 10px;
+
+    width: 585px;
+    height: 56px;
+
+    /* Inside auto layout */
+    flex: none;
+    order: 0;
+    align-self: stretch;
+    flex-grow: 0;
+  }
+
+  div#button {
+    /* ⚙️ Varier les propriétés - Bouton
+
+    Élément d'interaction de base.
+    Taille fixe recommandée.
+
+    Il est possible d'élargir le bouton, pour cela il faut changer la propriété "Resizing" du calque "Redimensionner ici" en "Fill container" et agrandir tout le composant.
+    */
+
+    /* Auto layout */
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+    padding: 0px;
+
+    width: 207px;
+    height: 56px;
+
+    border-radius: 0px 4px 0px 0px;
+
+    /* Inside auto layout */
+    flex: none;
+    order: 1;
+    flex-grow: 0;
+  }
+
+  button {
+    /* Redimensionner ici */
+
+    /* Auto layout */
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    padding: 12px 32px;
+    gap: 8px;
+
+    width: 207px;
+    height: 56px;
+
+    /* light/background/action-high-blue-france */
+    background: #000091;
+    border-radius: 0px 4px 0px 0px;
+
+    /* Inside auto layout */
+    flex: none;
+    order: 0;
+    align-self: stretch;
+    flex-grow: 0;
+  }
+
+  p#button {
+    /* Label bouton */
+
+    width: 111px;
+    height: 32px;
+
+    /* 2.Corps de texte/LG - Texte article/Desktop/Regular */
+    font-family: "Marianne-Regular";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 20px;
+    line-height: 32px;
+    /* identical to box height, or 160% */
+    text-align: center;
+
+    /* light/text/inverted-blue-france */
+    color: #f5f5fe;
+
+    /* Inside auto layout */
+    flex: none;
+    order: 1;
+    flex-grow: 0;
+  }
+
+  hr {
+    /* Bordure */
+
+    box-sizing: border-box;
+
+    width: 585px;
+    height: 0px;
+
+    /* light/border/plain-blue-france */
+    border: 2px solid #000091;
+
+    /* Inside auto layout */
+    flex: none;
+    order: 1;
+    align-self: stretch;
+    flex-grow: 0;
+  }
+
+  p#detail-recherche {
+    /* Egestas magna eu, condimentum amet. Vitae odio mauris suspendisse duis vestibulum. Nulla. */
+
+    width: 720px;
+    height: 24px;
+
+    /* 2.Corps de texte/MD - Texte standard/Desktop & Mobile - Regular */
+    font-family: "Marianne-Regular";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 24px;
+    /* identical to box height, or 150% */
+    text-align: center;
+
+    /* light/text/default-grey */
+    color: #3a3a3a;
+
+    /* Inside auto layout */
+    flex: none;
+    order: 2;
     flex-grow: 0;
   }
 </style>
